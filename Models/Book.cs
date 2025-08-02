d0a7abb0b2402b3bd06bdbbf011254c9cd2f3f56
Regina Angela Ramones <angela17ramones@gmail.com>
2025-08-02 14:10:01 +0800

    Delete Models directory

--- Models/Book.cs
+++
@@ -1,8 +0,0 @@
-namespace angelith.Models;
-
-public class Book
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; } 
-}
--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace angelith.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
